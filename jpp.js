11de284ff13d5a76e3c2f75fff346af18aa2f296
agogo <blank114@163.com>
2014-11-28 22:51:12 +0800

    start

---
+++ jpp.js
@@ -0,0 +1,49 @@
+;(function($,win,undefined){
+	
+	'use strict';
+
+	var defs = {
+			name:"Orz"
+	}
+
+	$.fn[pName]=function(options,value){
+		var name=defs.name;
+		if(typeof options === 'object' || !arguments.length){
+			var opts= $.extend({},defs,options);
+			return this.each(function(){
+				if (!$.data(this,'plugin-' + name)){
+						$.data(this,'plugin-' + name,new Plugin($(this),opts));
+				}
+			})
+		}
+	};
+
+	function Plugin(ele,opts){
+		this.$e = ele;
+		this.opts=opts;
+		this.init(opts);
+	};
+
+	Plugin.prototype={
+		constructor:Plugin,
+		init:function(opts){},
+		hide:function(){
+			this.$e.css('display','none')
+		},
+		show:function(){
+			this.$e.css('display','block')
+		},
+		destroy:function(){
+			$.data(this.$e[0],'plugin-' + pName,null);
+			$.removeData(this.$e[0],'plugin-' + pName);
+		}
+	}
+
+	$.fn.log = function (msg) {win.console && console.log && win.console.log("%s: %o", msg||'$', this,this.length);return this;};
+
+})(jQuery,window);
+
+
+
+$("p").Orz();
+
