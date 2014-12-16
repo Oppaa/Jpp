8e27348ebdceda09d9117fe133408d222cac2eda
oppaa <blank114@163.com>
2014-12-16 23:27:08 +0800

    removespace

--- jpp.js
+++ jpp.js
@@ -1,49 +1,47 @@
 ;(function($,win,undefined){
-	
-	'use strict';
+  
+  'use strict';
 
-	var defs = {
-			name:"Orz"
-	}
+  var defs = {
+    name:"Orz"
+  }
 
-	$.fn[pName]=function(options,value){
-		var name=defs.name;
-		if(typeof options === 'object' || !arguments.length){
-			var opts= $.extend({},defs,options);
-			return this.each(function(){
-				if (!$.data(this,'plugin-' + name)){
-						$.data(this,'plugin-' + name,new Plugin($(this),opts));
-				}
-			})
-		}
-	};
+  $.fn[pName]=function(options,value){
+    var name=defs.name;
+    if(typeof options === 'object' || !arguments.length){
+      var opts= $.extend({},defs,options);
+      return this.each(function(){
+        if (!$.data(this,'plugin-' + name)){
+            $.data(this,'plugin-' + name,new Plugin($(this),opts));
+        }
+      })
+    }
+  }
 
-	function Plugin(ele,opts){
-		this.$e = ele;
-		this.opts=opts;
-		this.init(opts);
-	};
+  function Plugin(ele,opts){
+    this.$e = ele;
+    this.opts=opts;
+    this.init(opts);
+  }
 
-	Plugin.prototype={
-		constructor:Plugin,
-		init:function(opts){},
-		hide:function(){
-			this.$e.css('display','none')
-		},
-		show:function(){
-			this.$e.css('display','block')
-		},
-		destroy:function(){
-			$.data(this.$e[0],'plugin-' + defs.name,null);
-			$.removeData(this.$e[0],'plugin-' + defs.name);
-		}
-	}
+  Plugin.prototype={
+    constructor:Plugin,
+    init:function(opts){},
+    hide:function(){
+      this.$e.css('display','none')
+    },
+    show:function(){
+      this.$e.css('display','block')
+    },
+    destroy:function(){
+      $.data(this.$e[0],'plugin-' + defs.name,null);
+      $.removeData(this.$e[0],'plugin-' + defs.name);
+    }
+  }
 
-	$.fn.log = function (msg) {win.console && console.log && win.console.log("%s: %o", msg||'$', this,this.length);return this;};
+  $.fn.log = function (msg) {win.console && console.log && win.console.log("%s: %o", msg||'$', this,this.length);return this;};
 
 })(jQuery,window);
-
-
 
 $("p").Orz();
 
